cfe2dced962eaef9358a922ce191e68f6c8a8687
McLenburg <>
2020-05-28 15:21:20 +0200

    added use of ffmpeg via bash

--- plutotv-generate.pl
+++ plutotv-generate.pl
@@ -11,6 +11,7 @@ use HTTP::Request ();
 use LWP::UserAgent;
 use URI::Escape;
 use UUID::Tiny ':std';
+use Cwd qw(cwd);
 
 package main;
 
@@ -28,6 +29,7 @@ my $response = $useragent->request($request);
 my $withm3u = grep { $_ eq '--createm3u'} @ARGV;
 # my $withgivenurl = grep { $_ eq '--usegivenurl'} @ARGV;
 my $useffmpeg = grep { $_ eq '--useffmpeg'} @ARGV;
+my $usebash = grep { $_ eq '--usebash'} @ARGV;
 my $jalle19 = grep { $_ eq '--usejalle19proxy'} @ARGV;  # https://github.com/Jalle19/node-ffmpeg-mpegts-proxy
 my $regionCode = "DE";  # may overwritten by stitched URL
 my $langcode ="de";
@@ -56,7 +58,7 @@ if ($response->is_success) {
     }
 
     my $pre = "";
-    my $uuid = uuid_to_string(create_uuid(UUID_V1));
+#    my $uuid = uuid_to_string(create_uuid(UUID_V1));
     my @senderListe = @{parse_json($response->decoded_content)};
     for my $sender( @senderListe ) {
       if($sender->{number} > 0) { 
@@ -65,7 +67,8 @@ if ($response->is_success) {
         $url =~ s/&deviceMake=/&deviceMake=Chrome/ig;
         $url =~ s/&deviceType=/&deviceType=web/ig;
         $url =~ s/&deviceModel=/&deviceModel=Chrome/ig;
-        $url =~ s/&sid=/&sid=$uuid/ig;
+        $url =~ s/&sid=/&sid=\{uuid\}/ig;
+#        $uuid = uuid_to_string(create_uuid(UUID_V1));
         my $regionStart = index($url, "marketingRegion=")+16;
         my $regionEnds = index($url, "&", index($url, "marketingRegion=")+16);
         if($regionStart>0) {
@@ -92,6 +95,19 @@ if ($response->is_success) {
                   print $fhj "\t}\n";
                   print $fhm "http://$jalleHost/".$sender->{_id}."\n";
                   $pre = ",";
+                }
+                elsif ( $usebash ) {
+                  my $path= cwd;
+                  print $fhm "pipe://".$path."/".$sender->{_id}.".sh\n";
+                  open(my $fhb, '>', $sender->{_id}.".sh") or die "Could not open file";
+                  print $fhb "#!/bin/bash\n";
+                  print $fhb "#\n\n";
+                  print $fhb "url=\"".$url."\"\n";
+                  print $fhb "uuid=\$(uuidgen)\n";
+                  print $fhb "repurl=\${url/\\{uuid\\}/\$uuid}\n";
+                  print $fhb "/usr/bin/ffmpeg -loglevel fatal -user-agent \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3554.0 Safari/537.36\" -i \$repurl  -vcodec copy -acodec copy -f mpegts -tune zerolatency -preset normal -metadata service_name='' pipe:1\n";
+                  close $fhb;
+                  chmod 0777, $sender->{_id}.".sh";
                 }
                 else {	
 		          print $fhm $url."\n";
