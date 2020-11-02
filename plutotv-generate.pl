5ed0b6f6c57ba6d6c89d1306d3d576b8eac4cb73
mclenburg <>
2020-11-02 19:32:04 +0100

    testing

--- plutotv-generate.pl
+++ plutotv-generate.pl
@@ -170,8 +170,8 @@ if ($response->is_success) {
                   $filename=~s/ /_/ig;
                   $filename=~s/\'//ig;
                   $filename=~s/\//_/ig;
-                    $filename=~s/(//ig;
-                    $filename=~s/)//ig;
+                    $filename=~s/\(//ig;
+                    $filename=~s/\)//ig;
                   create_bashfile ($sender, $url, $uuid, $filename);
                   print $fhm "pipe://".$programpath."/".$filename.".sh \n";
                 }
@@ -184,8 +184,8 @@ if ($response->is_success) {
               $filename=~s/ /_/ig;
               $filename=~s/\'//ig;
               $filename=~s/\//_/ig;
-              $filename=~s/(//ig;
-              $filename=~s/)//ig;
+              $filename=~s/\(//ig;
+              $filename=~s/\)//ig;
               create_bashfile( $sender, $url, $uuid, $filename);
           }
       }
