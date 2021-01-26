2780d58fb022079ef0cf6e969336380dbdf280c3
mclenburg <>
2021-01-26 19:09:16 +0100

    correct some errors

--- plutotv-localserver.pl
+++ plutotv-localserver.pl
@@ -17,19 +17,22 @@ my $port   = "9000";
 sub process_request {
     my $deamon = shift;
     my $client = $deamon->accept or die("could not get any Client");
-    my $request = $client->get_request(1) or die("could not get Client-Request.");
+    my $request = $client->get_request() or die("could not get Client-Request.");
     $client->autoflush(1);
 
     my $parse_params = HTTP::Request::Params->new({
         req => $request,
     });
-    my %params = $parse_params->params;
-    my $channelid = %params->{'channelid'};
+    my $params = $parse_params->params;
+    my $channelid = $params->{'channelid'};
 
     my $url = "";
-    for my $param (keys %params) {
-       $url+="&".%params->{$param};
+    for my $param (keys %{$params}) {
+        if ($param ne "channelid") {
+            $url .= "&" . $param . "=" . $params->{$param};
+        }
     }
+    $url = substr($url, 1);
     print($url);
 }
 
@@ -40,7 +43,7 @@ my $deamon = HTTP::Daemon->new(
     Reuse => 1,
     ReuseAddr => 1,
     ReusePort => $port,
-) or die ERROR "Server could not be started.\n\n";
+) or die "Server could not be started.\n\n";
 
 while (1) {
     process_request($deamon);
