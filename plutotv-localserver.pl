d0ed042cca5a825eff286b734d3f8f7ab035a569
mclenburg <>
2021-02-06 22:10:42 +0100

    server starting and react to request

--- plutotv-localserver.pl
+++ plutotv-localserver.pl
@@ -10,30 +10,47 @@ use HTTP::Status;
 use HTTP::Request::Params;
 use HTTP::Request::Common;
 use HTTP::Cookies;
+use DateTime;
+use DateTime::Format::Strptime qw(strptime);
 
 my $hostip = "127.0.0.1";
 my $port   = "9000";
+my $apiurl = "http://api.pluto.tv/v2/channels?start={from}Z&stop={to}Z";
 
 sub process_request {
+    my $from = DateTime->now();
+    my $to = $from->add(hours => 6);
+
+    $apiurl =~ s/{from}/$from/ig;
+    $apiurl =~ s/{to}/$to/ig;
+
     my $deamon = shift;
     my $client = $deamon->accept or die("could not get any Client");
     my $request = $client->get_request() or die("could not get Client-Request.");
     $client->autoflush(1);
 
-    my $parse_params = HTTP::Request::Params->new({
-        req => $request,
-    });
-    my $params = $parse_params->params;
-    my $channelid = $params->{'channelid'};
+    #http://localhost:9000/playlist <-- liefert m3u aus
+    #http://localhost:9000/channel?id=xxxx <-- liefert Stream des angefragten Senders
 
-    my $url = "";
-    for my $param (keys %{$params}) {
-        if ($param ne "channelid") {
-            $url .= "&" . $param . "=" . $params->{$param};
-        }
+    if($request->uri->path eq "/playlist") {
+        my $response = HTTP::Response->parse("This is playlist-response." );
+
+        $client->send_response($response);
     }
-    $url = substr($url, 1);
-    print($url);
+    elsif($request->uri->path eq "/channel") {
+        my $parse_params = HTTP::Request::Params->new({
+            req => $request,
+        });
+        my $params = $parse_params->params;
+        my $channelid = $params->{'id'};
+
+        my $response = HTTP::Response->parse("This is channel-response with id $channelid." );
+
+        $client->send_response($response);
+    }
+    else {
+        $client->send_error(RC_NOT_FOUND, "no such path available: ".$request->uri->path);
+    }
 }
 
 # START DAEMON
