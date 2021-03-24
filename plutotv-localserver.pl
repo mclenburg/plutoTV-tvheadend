6150182e9c71f75660d79bbef813a25549cdfd00
mclenburg <>
2021-03-24 20:31:33 +0100

    try: ffmpeg-convertion, mime-type for playlist

--- plutotv-localserver.pl
+++ plutotv-localserver.pl
@@ -141,7 +141,8 @@ sub buildM3U {
             my $logo = $sender->{logo}->{path};
             if(defined $logo) {
                 $m3u = $m3u . "#EXTINF:-1 tvg-chno=\"" . $sender->{number} . "\" tvg-id=\"" . uri_escape($sender->{name}) . "\" tvg-name=\"" . $sender->{name} . "\" tvg-logo=\"" . $logo . "\" group-title=\"PlutoTV\"," . $sender->{name} . "\n";
-                $m3u .= "pipe://" . $ffmpeg . " -loglevel fatal -threads 2 -re -fflags +genpts+ignidx+igndts -dts_delta_threshold 3000000 -i \"http://" . $hostip . ":" . $port . "/channel?id=" . $sender->{_id} . "\" -vcodec copy -acodec copy -f mpegts -tune zerolatency -metadata service_name=\"" . $sender->{name} . "\" pipe:1\n";
+                #$m3u .= "pipe://" . $ffmpeg . " -loglevel fatal -threads 2 -re -fflags +genpts+ignidx+igndts -dts_delta_threshold 3000000 -i \"http://" . $hostip . ":" . $port . "/channel?id=" . $sender->{_id} . "\" -vcodec copy -acodec copy -f mpegts -tune zerolatency -metadata service_name=\"" . $sender->{name} . "\" pipe:1\n";
+                $m3u .= "pipe://$ffmpeg -loglevel fatal -fflags +genpts+ignidx+igndts -i http://".$hostip.":".$port."/channel?id=$sender->{_id} -c copy -bsf:a aac_adtstoasc -tune zerolatency -metadata service_name=\"$sender->{name}\" pipe:1\n";
             }
         }
     }
@@ -193,6 +194,7 @@ sub send_m3ufile {
     }
     my $m3uContent = buildM3U(@senderListe);
     my $response = HTTP::Response->new();
+    $response->header("content-type", "audio/x-mpegurl");
     $response->header("content-disposition", "filename=\"plutotv.m3u\"");
     $response->code(200);
     $response->message("OK");
