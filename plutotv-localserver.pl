4f1f2a82a02ae2b137cfa149654f1a5f755f7e13
mclenburg <binimmerich@gmail.com>
2025-08-04 14:54:39 +0200

    try agein...

--- plutotv-localserver.pl
+++ plutotv-localserver.pl
@@ -42,19 +42,13 @@ our $cached_channels_time : shared = 0;
 our $cached_epg : shared;
 our $cached_epg_time : shared = 0;
 
-# Aenderung: $ua wird wieder global deklariert, aber OHNE das shared-Attribut.
-# Dies behebt den Fehler "Invalid value for shared scalar" und stellt die ursprüngliche
-# Funktionsweise wieder her, da es vor dem Start der Threads initialisiert wird.
-my $ua = LWP::UserAgent->new(
-    keep_alive => 1,
-    agent      => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:86.0) Gecko/20100101 Firefox/86.0'
-);
+# Aenderung: Es gibt kein globales $ua-Objekt mehr. Es wird pro Thread erstellt.
 
 # Kartierung von Regionen zu Breitengrad/Längengrad
 my %regions = (
-    'DE' => { lat => '52.5200', lon => '13.4050' },  # Berlin, Deutschland
-    'US' => { lat => '34.0522', lon => '-118.2437' }, # Los Angeles, USA
-    'UK' => { lat => '51.5074', lon => '-0.1278' },  # London, UK
+    'DE' => { lat => '52.5200', lon => '13.4050' },
+    'US' => { lat => '34.0522', lon => '-118.2437' },
+    'UK' => { lat => '51.5074', lon => '-0.1278' },
 );
 
 # Funktion zum Parsen der Kommandozeilenargumente
@@ -62,7 +56,7 @@ sub parse_args {
     my $localonly = 0;
     my $usestreamlink = 0;
     my $port = 9000;
-    my $region = 'DE'; # Standardregion
+    my $region = 'DE';
 
     GetOptions(
         "localonly"     => \$localonly,
@@ -79,16 +73,18 @@ sub parse_args {
     );
 }
 
-# Aenderung: Die Funktion verwendet jetzt das globale $ua-Objekt.
+# Aenderung: get_from_url akzeptiert jetzt ein $ua-Objekt
 sub get_from_url {
-    my ($url) = @_;
+    my ($ua_thread, $url) = @_;
     my $request = HTTP::Request->new(GET => $url);
-    my $response = $ua->request($request);
+    my $response = $ua_thread->request($request);
     return $response->is_success ? $response->content : undef;
 }
 
+# Aenderung: get_channel_json akzeptiert jetzt ein $ua-Objekt
 sub get_channel_json {
-    # Caching-Logik
+    my ($ua_thread) = @_;
+
     my $now = time();
     lock($cached_channels);
     lock($cached_channels_time);
@@ -101,13 +97,13 @@ sub get_channel_json {
     printf("Fetching fresh channel list from PlutoTV API.\n");
 
     my $from_ts = time();
-    my $to_ts = $from_ts + (2 * 24 * 60 * 60); # 2 Tage in der Zukunft
+    my $to_ts = $from_ts + (2 * 24 * 60 * 60);
     my $from_iso = strftime('%Y-%m-%dT%H:%M:%S', gmtime($from_ts));
     my $to_iso = strftime('%Y-%m-%dT%H:%M:%S', gmtime($to_ts));
 
     my $url = "$apiurl?start=${from_iso}Z&stop=${to_iso}Z";
     my $request = HTTP::Request->new(GET => $url);
-    my $response = $ua->request($request);
+    my $response = $ua_thread->request($request);
 
     unless ($response->is_success) {
         warn "Failed to fetch channel list: " . $response->status_line;
@@ -120,18 +116,18 @@ sub get_channel_json {
         return ();
     }
 
-    # Verwenden Sie share(), um die Datenstruktur vor der Zuweisung zu teilen
     $cached_channels = share($json_data);
     $cached_channels_time = $now;
     return @$json_data;
 }
 
+# Aenderung: getBootFromPluto akzeptiert jetzt ein $ua-Objekt
 sub getBootFromPluto {
-    my ($lat, $lon) = @_; # Breitengrad und Längengrad als Argumente
+    my ($ua_thread, $lat, $lon) = @_;
     printf("Refreshing current Session for coordinates: %s, %s\n", $lat, $lon);
     my $url = "https://boot.pluto.tv/v4/start?deviceId=$deviceid&deviceMake=Firefox&deviceType=web&deviceVersion=86.0&deviceModel=web&DNT=0&appName=web&appVersion=5.15.0-cb3de003a5ed7a595e0e5a8e1a8f8f30ad8ed23a&clientID=$deviceid&clientModelNumber=na&deviceLat=$lat&deviceLon=$lon";
     my $request = HTTP::Request->new(GET => $url);
-    my $response = $ua->request($request);
+    my $response = $ua_thread->request($request);
     unless ($response->is_success) {
         warn "Failed to get boot data: " . $response->status_line;
         return;
@@ -148,8 +144,9 @@ sub getBootFromPluto {
     return $session;
 }
 
+# Aenderung: get_bootJson akzeptiert jetzt ein $ua-Objekt
 sub get_bootJson {
-    my ($lat, $lon) = @_; # Übernimmt die Koordinaten vom Aufrufer
+    my ($ua_thread, $lat, $lon) = @_;
     my $now = time();
     lock($session);
     lock($bootTime);
@@ -158,7 +155,7 @@ sub get_bootJson {
     my $maxTime = $bootTime + $restartThresholdSec;
 
     unless (defined $session && $now <= $maxTime) {
-        return getBootFromPluto($lat, $lon);
+        return getBootFromPluto($ua_thread, $lat, $lon);
     }
     return $session;
 }
@@ -181,8 +178,8 @@ sub send_help {
 }
 
 sub send_xmltvepgfile {
-    my ($client, $request) = @_;
-    my @senderListe = get_channel_json();
+    my ($client, $request, $ua_thread) = @_;
+    my @senderListe = get_channel_json($ua_thread);
     unless (@senderListe) {
         $client->send_error(RC_INTERNAL_SERVER_ERROR, "Unable to fetch channel-list from pluto.tv-api.");
         return;
@@ -238,7 +235,7 @@ sub send_xmltvepgfile {
 }
 
 sub buildM3U {
-    my @senderliste = @_;
+    my ($ua_thread, @senderliste) = @_;
     my $m3u = "#EXTM3U\n";
     for my $sender (@senderliste) {
         next unless $sender->{number} > 0 && $sender->{number} != 2000;
@@ -259,13 +256,13 @@ sub buildM3U {
 }
 
 sub send_m3ufile {
-    my ($client) = @_;
-    my @senderListe = get_channel_json();
+    my ($client, $ua_thread) = @_;
+    my @senderListe = get_channel_json($ua_thread);
     unless (@senderListe) {
         $client->send_error(RC_INTERNAL_SERVER_ERROR, "Unable to fetch channel-list from pluto.tv-api.");
         return;
     }
-    my $m3uContent = buildM3U(@senderListe);
+    my $m3uContent = buildM3U($ua_thread, @senderListe);
     my $response = HTTP::Response->new(200, 'OK');
     $response->header("Content-Type", "audio/x-mpegurl");
     $response->header("Content-Disposition", "attachment; filename=\"plutotv.m3u8\"");
@@ -283,17 +280,17 @@ sub fixPlaylistUrlsInMaster {
 }
 
 sub send_playlistm3u8file {
-    my ($client, $request) = @_;
+    my ($client, $request, $ua_thread) = @_;
     my $params = HTTP::Request::Params->new({ req => $request })->params;
     my ($playlistid, $channelid, $sessionid) = ($params->{'id'}, $params->{'channelid'}, $params->{'session'});
     unless ($playlistid && $channelid && $sessionid) {
         $client->send_error(RC_BAD_REQUEST, "Missing required parameters.");
         return;
     }
-    my $bootJson = get_bootJson($regions{DE}->{lat}, $regions{DE}->{lon});
+    my $bootJson = get_bootJson($ua_thread, $regions{DE}->{lat}, $regions{DE}->{lon});
     my $getparams = "terminate=false&embedPartner=&serverSideAds=false&paln=&includeExtendedEvents=false&architecture=&deviceId=unknown&deviceVersion=unknown&appVersion=unknown&deviceType=web&deviceMake=Firefox&sid=".$sessionid."&advertisingId=&deviceLat=54.1241&deviceLon=12.1247&deviceDNT=0&deviceModel=web&userId=&appName=";
     my $url = $bootJson->{servers}->{stitcher}."/stitch/hls/channel/".$channelid."/".$playlistid."/playlist.m3u8?".$getparams;
-    my $playlist = get_from_url($url);
+    my $playlist = get_from_url($ua_thread, $url);
     unless (defined $playlist) {
         $client->send_error(RC_INTERNAL_SERVER_ERROR, "Failed to fetch playlist.");
         return;
@@ -306,17 +303,17 @@ sub send_playlistm3u8file {
 }
 
 sub send_masterm3u8file {
-    my ($client, $request) = @_;
+    my ($client, $request, $ua_thread) = @_;
     my $params = HTTP::Request::Params->new({ req => $request })->params;
     my $channelid = $params->{'id'};
     unless ($channelid) {
         $client->send_error(RC_BAD_REQUEST, "Missing channel ID.");
         return;
     }
-    my $bootJson = get_bootJson($regions{DE}->{lat}, $regions{DE}->{lon});
+    my $bootJson = get_bootJson($ua_thread, $regions{DE}->{lat}, $regions{DE}->{lon});
     my $baseurl = $bootJson->{servers}->{stitcher}."/stitch/hls/channel/".$channelid."/";
     my $url = $baseurl."master.m3u8?".$bootJson->{stitcherParams};
-    my $master = get_from_url($url);
+    my $master = get_from_url($ua_thread, $url);
     unless (defined $master) {
         $client->send_error(RC_INTERNAL_SERVER_ERROR, "Failed to fetch master playlist.");
         return;
@@ -330,8 +327,8 @@ sub send_masterm3u8file {
 }
 
 sub send_channels_json {
-    my ($client) = @_;
-    my @senderListe = get_channel_json();
+    my ($client, $ua_thread) = @_;
+    my @senderListe = get_channel_json($ua_thread);
     my $response = HTTP::Response->new(200, 'OK');
     $response->header("Content-Type", "application/json");
     $response->content(encode_json(\@senderListe));
@@ -339,7 +336,7 @@ sub send_channels_json {
 }
 
 sub search_channels {
-    my ($client, $request) = @_;
+    my ($client, $request, $ua_thread) = @_;
     my $params = HTTP::Request::Params->new({ req => $request })->params;
     my $query = lc($params->{'q'} || '');
 
@@ -348,7 +345,7 @@ sub search_channels {
         return;
     }
 
-    my @senderListe = get_channel_json();
+    my @senderListe = get_channel_json($ua_thread);
     my @results;
 
     for my $sender (@senderListe) {
@@ -364,8 +361,8 @@ sub search_channels {
 }
 
 sub get_categories {
-    my ($client) = @_;
-    my @senderListe = get_channel_json();
+    my ($client, $ua_thread) = @_;
+    my @senderListe = get_channel_json($ua_thread);
     my %categories;
     for my $sender (@senderListe) {
         if ($sender->{category}) {
@@ -380,8 +377,13 @@ sub get_categories {
     $client->send_response($response);
 }
 
+# Aenderung: process_request erstellt das $ua-Objekt und reicht es weiter
 sub process_request {
     my $client = shift;
+    my $ua_thread = LWP::UserAgent->new(
+        keep_alive => 1,
+        agent      => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:86.0) Gecko/20100101 Firefox/86.0'
+    );
     my $request = eval { $client->get_request() };
     unless (defined $request) {
         warn "Could not get client request.\n";
@@ -391,19 +393,19 @@ sub process_request {
     my $path = $request->uri->path;
     printf("Request received for path %s\n", $path);
     if ($path eq "/playlist") {
-        send_m3ufile($client);
+        send_m3ufile($client, $ua_thread);
     } elsif ($path eq "/master3u8") {
-        send_masterm3u8file($client, $request);
+        send_masterm3u8file($client, $request, $ua_thread);
     } elsif ($path eq "/playlist3u8") {
-        send_playlistm3u8file($client, $request);
+        send_playlistm3u8file($client, $request, $ua_thread);
     } elsif ($path eq "/epg") {
-        send_xmltvepgfile($client, $request);
+        send_xmltvepgfile($client, $request, $ua_thread);
     } elsif ($path eq "/channels") {
-        send_channels_json($client);
+        send_channels_json($client, $ua_thread);
     } elsif ($path eq "/search") {
-        search_channels($client, $request);
+        search_channels($client, $request, $ua_thread);
     } elsif ($path eq "/categories") {
-        get_categories($client);
+        get_categories($client, $ua_thread);
     } elsif ($path eq "/") {
         send_help($client);
     } else {
@@ -420,7 +422,6 @@ sub sig_handler {
 
 # Hauptfunktion
 sub main {
-    # Signal-Handler registrieren
     $SIG{INT} = 'sig_handler';
     $SIG{TERM} = 'sig_handler';
 
@@ -459,7 +460,12 @@ sub main {
     printf("Using %s for streaming\n", $usestreamlink ? "streamlink" : "ffmpeg");
     printf("Pluto TV content is being fetched for region '%s'\n", $args{region});
 
-    get_bootJson($lat, $lon);
+    # Für die erste Initialisierung der Session (bevor Threads starten)
+    my $init_ua = LWP::UserAgent->new(
+        keep_alive => 1,
+        agent      => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:86.0) Gecko/20100101 Firefox/86.0'
+    );
+    get_bootJson($init_ua, $lat, $lon);
 
     while (my $client = $daemon->accept) {
         threads->new(\&process_request, $client)->detach();
